973bc22e5f52709fee79998b6e8409bb6896c557
Kinga Zawarty <niu.ula.a@gmail.com>
2025-08-07 13:42:05 +0200

    basic game

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -1,24 +1,59 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Cell from './components/tictactoe/Cell';
 
 function App() {
+
+  const [cells, setCells] = useState(["", "", "","", "", "","", "", ""])
+  const [go, setGo] = useState("circle");
+  const [winningMessage, setWinningMessage] = useState("");
+
+  const message = "it is now " + go + " turn ";
+
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ]
+
+    winningCombos.forEach(array => {
+      let circleWins = array.every(cell => cells[cell] === "circle");
+      if(circleWins) setWinningMessage("circles win");
+      return;
+    })
+
+    winningCombos.forEach(array => {
+      let circleWins = array.every(cell => cells[cell] === "cross");
+      if(circleWins) setWinningMessage("cross win");
+      return;
+    })
+  }
+
+  useEffect(() =>  {
+    checkScore();
+  }, [cells])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="gameboard">
+          {cells.map((cell, index) => 
+          <Cell
+            key = {index}
+            id = {index}
+            cell = {cell}
+            go = {go}
+            setGo = {setGo}
+            cells = {cells}
+            setCells = {setCells}
+            winningMessage= {winningMessage}
+          />)}
+      </div>
+
+      <p> {winningMessage || message} </p>
+      
     </div>
   );
 }
