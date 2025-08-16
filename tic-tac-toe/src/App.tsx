2d90e9a3fd54fec935f623c089d908b0b75ed0c8
Kinga Zawarty <niu.ula.a@gmail.com>
2025-08-16 14:06:48 +0200

    add lofi background music

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import TicTacToe from "./games/tictactoe/TicTacToeGame";
 import GameButtonsContainer from "./main_page/components/GameButtons/GamesButtonsContainer";
 import PufferFish from './main_page/components/Decorations/PufferFish';
+import Music from './main_page/components/Decorations/Music';
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
       <video autoPlay loop muted playsInline className="bg-video">
         <source src="/main_page_background_video.mp4" type="video/mp4" />
       </video>
+
+      <Music/>
 
 
       {selectedGame === "tictactoe" ? (
---
+++ tic-tac-toe/src/main_page/components/Decorations/Music.tsx
@@ -0,0 +1,6 @@
+
+const Music = () => (
+  <audio src="/lofi_background.mp3" autoPlay loop /> // controls to show player
+);
+
+export default Music;
